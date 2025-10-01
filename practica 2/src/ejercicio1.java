3186de8de3e78bf05b297a9e60ee5a0b579a595e
practica 1 <adrianqr21@gmail.com>
2025-10-01 14:21:08 +0200

    ejercicio1 empezado

--- practica 2/src/ejercicio1.java
+++ practica 2/src/ejercicio1.java
@@ -1,2 +1,6 @@
 public class ejercicio1 {
+}   public static void main(String[] args){
+        System.out.println("ejercicio de la pactica 2")
+
+                System.out.println("Final de la practica")
 }
