c7b3c0f7fbe1b85820297c7006784dadc285ea25
AdrianQR21 <adrianqr21@gmail.com>
2025-10-06 14:21:39 +0200

    Delete practica 2/src directory

--- practica 2/src/ejercicio1.java
+++
@@ -1,6 +0,0 @@
-public class ejercicio1 {
-}   public static void main(String[] args){
-        System.out.println("ejercicio de la pactica 2")
-
-                System.out.println("Final de la practica")
-}
