f516238d3117d357e47245f9e864352ce98aea3d
practica 1 <adrianqr21@gmail.com>
2025-10-06 14:19:59 +0200

    ejercicio1

--- practica 2/src/ejercicio1.java
+++ practica 2/src/ejercicio1.java
@@ -1,6 +1,26 @@
-public class ejercicio1 {
-}   public static void main(String[] args){
-        System.out.println("ejercicio de la pactica 2")
+import jdk.jshell.EvalException;
 
-                System.out.println("Final de la practica")
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    //Crea un array de 10 posiciones de números con valores pedidos
+    // por teclado. Muestra por consola el indice y el valor al que
+    // corresponde.
+
+    public static void main(String[] args) {
+        //Crea un array de 10 posiciones de numeros con valores pedidos por teclado.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce otro numero");
+        int[] arrayInt = new int[10];
+        for (int i = 0; i < arrayInt.length; i++) {
+
+            arrayInt[i] = sc.nextInt();
+
+            //Muestra por consola el indice y el valor al que corresponde.
+        }
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.println("indice " + i + " --> " + arrayInt[i]);
+        }
+    }
 }
