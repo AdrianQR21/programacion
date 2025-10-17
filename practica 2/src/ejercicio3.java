9fc1d74e764ce089437579c91d5b77a198da48ec
practica 1 <adrianqr21@gmail.com>
2025-10-17 09:05:42 +0200

    ejercicio3

---
+++ practica 2/src/ejercicio3.java
@@ -0,0 +1,13 @@
+public class ejercicio3 {
+    //Crea un array que contenga 5 números. Realiza un programa que
+    // te muestre por pantalla SOLO el más pequeño de ellos.
+    public static void main(String[] args) {
+        int[] x = {23, 2, 7, 10, 11};
+        int menor = x[0];
+        for (int i = 1; i < x.length; i++)
+           if (x[i] < menor) {
+                menor = x[i];
+           }
+        System.out.println("El número mas pequeño es:" + menor);
+    }
+}
