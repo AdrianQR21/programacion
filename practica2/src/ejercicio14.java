d97e90b755c4b7322c55bdb6ff16867b3525ff5f
practica 1 <adrianqr21@gmail.com>
2025-10-24 09:59:53 +0200

    Ejercicio 14 completado correctamente

--- practica2/src/ejercicio14.java
+++ practica2/src/ejercicio14.java
@@ -10,28 +10,26 @@ import java.util.Scanner;
 
 public class ejercicio14 {
     public static void main(String[] args) {
-        //definimos la clase Scanner y el arraylist donde vamos a guardar los nombres y la entrada de datos
+        //definimos donde vamos a guardar los nombres y la entrada de datos
         ArrayList<String> lista = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         String entrada;
 
-        //vamos pidiendo nombres hasta que no sea igual a fin la entrada de datos
+        //vamos dando nombres hasta que no sea igual a fin
         do {
             System.out.println("Introduce un nombre (fin = salir)");
             entrada = s.nextLine();
 
-            //Si no es igual a fin entonces añade el nombre a la entrada
+            //Si no es igual a fin entonces añadirá el nombre a la entrada
             if (!entrada.equals("fin")){
                 lista.add(entrada);
             }
         } while (!entrada.equals("fin"));
 
-        //Pedimos por pantalla el nombre a buscar
+        //Imprimimos por pantalla el nombre que vamos a buscar
         System.out.println("dime un nombre a buscar: ");
         String entradaNom= s.nextLine();
 
-        //ahora vemos con el metodo .contains() si existe dentro de nuestro arraylist
-        //si lo ha encontrado sacaremos el índice del elemento que ha encontrado, si no, mostraremos que no existe ese elemento en especifico
         if (lista.contains(entradaNom)){
             System.out.printf("El nombre existe y se encuentra en la posición %d",lista.indexOf(entradaNom));
         } else {
