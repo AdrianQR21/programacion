cc4a392ece2a65b04a8d25b587ee518b3731c1ab
practica 1 <adrianqr21@gmail.com>
2026-01-14 14:20:03 +0100

    Habilidades y Personajes en modelo

---
+++ practica5/src/com/juego/Habilidades/HabilidadBase.java
@@ -0,0 +1,11 @@
+package com.juego.habilidades;
+
+import com.juego.Modelo.Personaje;
+
+public interface HabilidadBase {
+    //Para restar el uso de las habilidades
+    boolean usar();
+    int escaladoPoder(Personaje pj);
+    void infoHabilidad();
+    void nombreHabilidad();
+}
