964f36d19777e125c8a78225dce9857d41478ea0
practica 1 <adrianqr21@gmail.com>
2026-02-06 10:58:01 +0100

    Empezando practica 6

--- practica5/src/Main.java
+++ practica5/src/Main.java
@@ -2,7 +2,7 @@ import com.juego.modelo.juego;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Creamos una instancia de nuestro motor de juego.
+        // 1. Creamos una instancia de nuestro motor de juego.2
         juego miJuego = new juego();
 
         // 2. Le decimos que inicie.
