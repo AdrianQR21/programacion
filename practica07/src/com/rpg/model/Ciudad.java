09ce351e2d8e69917df1bcb73de4cce18b3543db
practica 1 <adrianqr21@gmail.com>
2026-03-04 13:24:13 +0100

    Seguimiento practica 07

---
+++ practica07/src/com/rpg/model/Ciudad.java
@@ -0,0 +1,47 @@
+package com.rpg.model;
+
+public class Ciudad {
+    String nombre;
+    int población;
+    String clima;
+    int nivelRiesgo;
+
+    public Ciudad(String nombre, int población, String clima, int nivelRiesgo) {
+        this.nombre = nombre;
+        this.población = población;
+        this.clima = clima;
+        this.nivelRiesgo = nivelRiesgo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPoblación() {
+        return población;
+    }
+
+    public void setPoblación(int población) {
+        this.población = población;
+    }
+
+    public String getClima() {
+        return clima;
+    }
+
+    public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public int getNivelRiesgo() {
+        return nivelRiesgo;
+    }
+
+    public void setNivelRiesgo(int nivelRiesgo) {
+        this.nivelRiesgo = nivelRiesgo;
+    }
+}
--- practica07/src/utils/TxtHelper.java
+++ practica07/src/utils/TxtHelper.java
@@ -1,4 +1,21 @@
 package utils;
 
+import com.rpg.model.Ciudad;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TxtHelper {
+    public static void leerCiudades(){
+        try {
+            List<String> lineas = Files.readAllLines(Paths.get("practica07/ficheros/ciudades.txt"));
+            List<Ciudad>listaCiudades = new ArrayList<>();
+        }catch (IOException e){
+            System.out.println();
+        }
+    }
+
 }
